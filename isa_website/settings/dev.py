6db98dfc103fd1f2df555fa810e51d61a1e0f0ef
timcknowles <tim.c.knowles@gmail.com>
2018-08-16 14:07:50 +0100

    remove secret key from source control and add heroku deployment stuff

--- isa_website/settings/dev.py
+++ isa_website/settings/dev.py
@@ -1,13 +1,15 @@
+from .secret_settings import *
 from .base import *
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9wxn71ybfjd59gq!geo(qs&&4f@%2m!2x15#x$phft5fl9*o20'
+SECRET_KEY = ['SECRET_KEY']
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
