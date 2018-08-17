0b1fcd89783d25d8f9fd63b4d0db6de81beed53e
timcknowles <tim.c.knowles@gmail.com>
2018-08-17 09:34:12 +0100

    add media url

--- isa_website/settings/production.py
+++ isa_website/settings/production.py
@@ -42,3 +42,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_HOST = 's3.amazonaws.com'
 S3_USE_SIGV4 = True
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
