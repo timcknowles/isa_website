f338eddafe6c23419b7bed6ca031e7c540090ebb
timcknowles <tim.c.knowles@gmail.com>
2018-08-19 12:55:36 +0100

    add custom domain to base

--- isa_website/settings/base.py
+++ isa_website/settings/base.py
@@ -150,6 +150,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
