2cec55f5449c477bf2b2a9af6177b51a1e04cde0
timcknowles <tim.c.knowles@gmail.com>
2018-08-19 13:00:05 +0100

    add bucket name to base

--- isa_website/settings/base.py
+++ isa_website/settings/base.py
@@ -151,6 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_STORAGE_BUCKET_NAME 'isawebsite'
 
 
 
