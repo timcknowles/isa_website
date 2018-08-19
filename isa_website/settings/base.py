492cb777b48367818c3abad6b7e77e19b764516b
timcknowles <tim.c.knowles@gmail.com>
2018-08-19 13:06:57 +0100

    add

--- isa_website/settings/base.py
+++ isa_website/settings/base.py
@@ -149,12 +149,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_STORAGE_BUCKET_NAME = 'isawebsite'
 
 
-
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
