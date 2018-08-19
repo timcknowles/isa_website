24258c12449f88fbdd3d2bf23c8827ee44017076
timcknowles <tim.c.knowles@gmail.com>
2018-08-19 13:50:17 +0100

    test removing query string

--- isa_website/settings/base.py
+++ isa_website/settings/base.py
@@ -157,6 +157,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_HOST = 's3.amazonaws.com'
 S3_USE_SIGV4 = True
+AWS_QUERYSTRING_AUTH = False
 
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
