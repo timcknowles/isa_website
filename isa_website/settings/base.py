a055a3a272cba718ac308c9ab7f977a7326fa43d
timcknowles <tim.c.knowles@gmail.com>
2018-08-19 13:29:39 +0100

    put amazon keys into local env

--- isa_website/settings/base.py
+++ isa_website/settings/base.py
@@ -150,10 +150,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
 
 
-
+AWS_S3_REGION_NAME = env['REGION_NAME']
 AWS_STORAGE_BUCKET_NAME = 'isawebsite'
+AWS_ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
+AWS_S3_HOST = 's3.amazonaws.com'
+S3_USE_SIGV4 = True
 
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
