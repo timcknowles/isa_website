5ae2160f81e9be992240fd330375e6c552529fc4
timcknowles <tim.c.knowles@gmail.com>
2018-10-31 11:54:23 +0000

    remove crispy forms from reg

---
+++ isa_website/forms.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, get_user_model
+from registration.forms import RegistrationFormTermsOfService, RegistrationFormUniqueEmail
+from django.utils.translation import ugettext_lazy as _
+
+from wagtail.users.forms import UserEditForm, UserCreationForm
+
+User = get_user_model()
+
+class CustomForm(RegistrationFormTermsOfService, RegistrationFormUniqueEmail):
+  first_name = forms.CharField(required=True, label=_('First Name'))
+  last_name = forms.CharField(required=True, label=_('Last Name'))
+
+
+  pass
