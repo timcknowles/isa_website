da226e915cf9c396f52f7a17609c56e4c7ca555a
timcknowles <tim.c.knowles@gmail.com>
2018-12-06 18:31:53 +0000

    standard page model and template

--- home/models.py
+++ home/models.py
@@ -7,6 +7,11 @@ from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.documents.edit_handlers import DocumentChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.embeds.blocks import EmbedBlock
 
 
 
@@ -15,6 +20,26 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+    ]
+
+
+class StandardPage(Page):
+
+    section = StreamField([
+        ('sub_heading', blocks.CharBlock(classname="sub_heading")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('quote', blocks.BlockQuoteBlock()),
+        ('image', ImageChooserBlock()),
+        ('document', DocumentChooserBlock()),
+        ('embed', EmbedBlock()),
+        ('table', TableBlock()),
+    ], blank=True)
+
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('section')
+
+
     ]
 
 class EventPage(Page):
