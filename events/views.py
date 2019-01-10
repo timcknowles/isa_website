247aff2af0e8c49032f3c438d670ab5560ee95a8
timcknowles <tim.c.knowles@gmail.com>
2019-01-10 21:38:58 +0000

    another json error

--- events/views.py
+++ events/views.py
@@ -1,35 +1,53 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from eventbrite import Eventbrite
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
-
-
+@require_POST
 @csrf_exempt
 def eventbrite(request):
-    return JsonResponse (request.body,safe=False)
-    data = json.loads(request)
-    api_url = data.get('api_url', None)
+    # json_string = request
+    data = json.loads(request.body)
+    # data = json.dumps(json_string)
+    # print(data)
 
-    event = Event(api_url)
-    event.save()
-    return HttpResponse("webhook received by ISA")
-
-
-
-# @app.route('/eventbrite', methods=['POST'])
-def webhook():
+    print(data)
+    return HttpResponse('success')
 
 
-    # Use the API client to convert from a webhook to an API object
-    api_object = eventbrite.webhook_to_object(request)
 
-    # Process the API object
-    if api_object.type == 'User':
-        do_user_process(api_object)
+    # api_url = data["api_url"]
+    #
+    # print(api_url)
 
-    if api_object.type == 'Event':
-        do_event_process(api_object)
 
-    return ""
+
+# def eventbrite(request):
+#     return JsonResponse (request.body,safe=False)
+#     data = json.loads(request)
+#     api_url = data.get('api_url', None)
+#
+#     event = Event(api_url)
+#     event.save()
+#     return HttpResponse("webhook received by ISA")
+
+
+
+# def webhook():
+#
+#
+#     # Use the API client to convert from a webhook to an API object
+#     api_object = eventbrite.webhook_to_object(request)
+#
+#     # Process the API object
+#     if api_object.type == 'User':
+#         do_user_process(api_object)
+#
+#     if api_object.type == 'Event':
+#         do_event_process(api_object)
+#
+#         print(api_object)
+#
+#     return ""
