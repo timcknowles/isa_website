6f7f892fd76affb97925f2a93d4c2ced74988691
timcknowles <tim.c.knowles@gmail.com>
2019-11-07 13:21:07 +0000

    add crispy forms to blog submit

--- news/forms.py
+++ news/forms.py
@@ -9,15 +9,24 @@ from wagtail.users.forms import UserEditForm, UserCreationForm
 from news.models import NewsPage
 
 
+
+
 class BlogForm(forms.ModelForm):
     class Meta:
         model = NewsPage
         labels = {
             'title': 'title',
-            'summary': 'summary'
+            'intro': 'one line summary',
+            'summary': 'summary',
+            'first_name': 'first name',
+            'last_name': 'last name',
+            'email': 'email',
+            'contact_number': 'contact number'
+
+
         }
         fields = [
-            'title', 'summary'
+            'title','intro', 'summary', 'first_name','last_name','email', 'contact_number'
         ]
     class Media:
         css =('all')
